3aa60dc9009f77d61057b8df8761ce3703892497
tomaszpankowski <pankowski.email@gmail.com>
2022-02-06 21:16:38 +0100

    layout done

--- src/views/events.js
+++ src/views/events.js
@@ -1,11 +1,16 @@
 import React,{Component} from "react";
 import Container from "../../node_modules/react-bootstrap/Container";
+import EventsS1 from "../components/events-s1";
+import EventsS2 from "../components/events-s2";
+import EventsS3 from "../components/events-s3";
 
 class Events extends Component{
     render(){
         return(        
-            <Container fluid className="events-s1 d-flex align-items-center minh-100vh">
-                
+            <Container fluid className="minh-footer-adj p-0">
+                <EventsS1/>
+                <EventsS2/>
+                <EventsS3/>
             </Container>    
         );
     }
