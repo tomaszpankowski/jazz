f28ffff241d73c44a0d2951514054990039551f3
tomaszpankowski <pankowski.email@gmail.com>
2022-03-27 20:05:07 +0200

    frontend

--- src/components/index-s1.js
+++ src/components/index-s1.js
@@ -16,18 +16,18 @@ class IndexS1 extends Component{
             </Link>;
         }
         return(    
-            <Container fluid className="d-flex minh-footer-adj align-items-center py-5 ">
+            <Container fluid className="d-flex minh-100vh align-items-center py-5 ">
                 <Row className="mx-0 w-100 mt-5">
                     <Col xs={{span:12,order:1}} md={{span:7,order:2}} className="minh-25vh"></Col>
                     <Col xs={{span:12,order:2}} md={{span:5,order:1}} className="minh-50vh d-flex align-items-center">
-                        <div className="w-100 text-center text-md-start">
+                        <div className="w-100 text-center text-md-start text-shadow">
                             <h5 className="text-white">
                                 Sed ut perspiciatis
                             </h5>
-                            <h2 className="dispaly-6 fw-bold text-white-50 text-shadow">
+                            <h2 className="dispaly-6 fw-bold text-white-50">
                                 Unde omnis iste
                             </h2>
-                            <p className="text-white">
+                            <p className="text-white lead">
                                 Natus error sit voluptatem accusantium doloremque laudantium, 
                                 totam rem aperiam, eaque ipsa quae ab illo inventore veritatis 
                                 et quasi architecto beatae vitae dicta sunt explicabo. 
